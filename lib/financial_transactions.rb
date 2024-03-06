0222aea627e18ce743396e7b92b92b5a95fe04a1
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2024-03-06 12:39:27 -0500

    Initial commit

---
+++ lib/financial_transactions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "financial_transactions/version"
+
+module FinancialTransactions
+  class Error < StandardError; end
+  # Your code goes here...
+end
